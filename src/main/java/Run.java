b7f4f3b72ee4949fcceed02958855ccecab9cda4
Mantas Gricius <mantasgricius6@gmail.com>
2024-05-10 13:55:11 +0300

    clean data preprocessing in main

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,17 +1,33 @@
 import org.data.preprocessing.DataFilter;
 import org.data.preprocessing.DataMerger;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 public class Run {
     public static void main(String[] args) throws Exception {
-//        DataMerger merger = new DataMerger("/MON/", "MONMerged.csv", true);
-//        merger.Merge();
+        DataFilter roFilter = new DataFilter("RoFiltered_2.csv", "RoAggregated.csv", "|");
+        roFilter.AggregateData(1, 4);
+    }
+
+    public static void DataPreprocessing() throws URISyntaxException, IOException {
+        DataMerger merger = new DataMerger("/MON/", "MONMerged.csv", true);
+        merger.Merge();
+        merger.setDirectoryPath("/MGR/");
+        merger.setOutputFilePath("MGRMerged.csv");
+        merger.Merge();
+        merger.setDirectoryPath("/Ro/");
+        merger.setOutputFilePath("RoMerged.csv");
+        merger.setContainsHeader(false);
+        merger.Merge();
         DataFilter roFilter = new DataFilter("RoMerged.csv", "RoFiltered_1.csv", "|");
         roFilter.FilterColumns(Arrays.asList(0, 6, 44, 46, 54, 55));
         roFilter.setInputFileName("RoFiltered_1.csv");
         roFilter.setOutputFileName("RoFiltered_2.csv");
         roFilter.FilterSuccess(3);
-//        System.out.println("test");
+        roFilter.setInputFileName("RoFiltered_2.csv");
+        roFilter.setOutputFileName("RoAggregated.csv");
+        roFilter.AggregateData(1, 4);
     }
 }
